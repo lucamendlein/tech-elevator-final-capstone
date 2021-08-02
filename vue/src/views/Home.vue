c01f4a1405f6dff1be24e986822629e1fc3c8de5
Ryan Drake <ryanadrake4047@gmail.com>
2021-08-02 15:34:55 -0400

    empty commit

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
+  
+    
   </div>
+  
+  
 </template>
 
 <script>
+
 export default {
   name: "home"
 };
