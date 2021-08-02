9f8f23c708dc0816f4d1b4f4f62f87f373ed22ce
Ryan Drake <ryanadrake4047@gmail.com>
2021-08-02 15:45:30 -0400

    empty commit

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,15 +1,12 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-<<<<<<< HEAD
-  
-    
-=======
+
     <p>You must be authenticated to see this</p>
     <div>
       <splash-page-comp v-if="$store.state.token=''"/>
-    </div>
->>>>>>> 9e4a95a6c9cf32266d558f9c4b536115a5f18d29
+  </div>
+
   </div>
   
   
@@ -17,10 +14,8 @@
 
 <script>
 
-<<<<<<< HEAD
-=======
+
 import SplashPageComp from "@/components/SplashPageComp";
->>>>>>> 9e4a95a6c9cf32266d558f9c4b536115a5f18d29
 export default {
   name: "home",
   components: {SplashPageComp}
