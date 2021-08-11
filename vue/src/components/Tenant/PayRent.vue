34721866bf1195ad4d1b292d05e3481924755463
Ryan Drake <ryanadrake4047@gmail.com>
2021-08-11 00:31:21 -0400

    started on pay rent

---
+++ vue/src/components/Tenant/PayRent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <p>Your rent is due the 1st of every month and must be paid by the 4th of each month per rental agreement</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
