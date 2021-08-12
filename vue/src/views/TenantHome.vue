e1a7b599e6ed0fc0342d1ead723b75b387006159
Tekikil Metiku <tekiendalew@gmail.com>
2021-08-12 14:35:10 -0400

    tenant page fixed

--- vue/src/views/TenantHome.vue
+++ vue/src/views/TenantHome.vue
@@ -1,9 +1,20 @@
 <template>
-<div>
+  <div class="d-flex flex-row h-100 w-100">
+    <div class="side-bar">
 
- <router-link v-bind:to="{name: 'service-request'}"> <b-button  >Add Service Request</b-button></router-link>
- <router-link v-bind:to="{name: 'pay-rent'}"> <b-button>Pay Rent</b-button></router-link>
+      <Sidebar :options="side_bar_options"/>
+
+    </div>
+    <div class="right-side">
+      <img src="'../../imgs/logo.svg'">
+    </div>
+    <div>
+
+    </div>
+
+
   </div>
+
 </template>
 
 
@@ -11,11 +22,34 @@
 
 
 
+import Sidebar from "@/components/Navigation/Sidebar";
 export default {
+  components: {Sidebar},
+  data() {
+    return {
+      side_bar_options:{
+        title: "Dashboard",
+        links: [
+          {
+            name: 'Pay Rent',route : '/tenants/rent'
+          },
+          {
+            name: 'Service Request',route : '/work-orders'
+          },
 
+
+        ]
+      }
+    }
+  }
 }
 </script>
 
 <style>
+.side-bar {
+  background: white;
+  width: 400px;
+  height: 100%;
+}
 
 </style>
